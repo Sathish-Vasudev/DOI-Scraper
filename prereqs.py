f42d318782bfc47cf4b439f59a429c3c323920cd
Sathish Vasudev <Sathish-Vasudev@users.noreply.github.com>
2021-01-03 21:30:46 +0530

    Update prereqs.py

--- prereqs.py
+++ prereqs.py
@@ -1,14 +1,14 @@
 import subprocess
 import sys
 
-print("This pre-requisites program will install all the necessary modules.\n")
+print("This pre-requisites program will install all the necessary modules for DOI-Scraper.\n")
 
 def install(name):
     subprocess.call([sys.executable, '-m', 'pip', 'install', name])
     
 install("pandas")
 install("BeautifulSoup")
+install("xlsxwriter")
 
 print("\nAll the modules are installed !")
-print("You can now run the Article Scraper program!")
-
+print("You can now run the DOI-Scraper program!")
