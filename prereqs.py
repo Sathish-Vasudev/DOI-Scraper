df2837aa195b64b55996e5f367b7fc0a74522b21
Sathish Vasudev <Sathish-Vasudev@users.noreply.github.com>
2021-01-09 00:20:57 +0530

    Update prereqs.py

--- prereqs.py
+++ prereqs.py
@@ -7,7 +7,7 @@ def install(name):
     subprocess.call([sys.executable, '-m', 'pip', 'install', name])
     
 install("pandas")
-install("BeautifulSoup")
+install("BeautifulSoup4")
 install("xlsxwriter")
 
 print("\nAll the modules are installed !")
